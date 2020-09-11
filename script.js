c94fe7f5e13fd49609dc1ea2b2d84988d0860b28
Gil Marsh <marsh.skc@gmail.com>
2020-09-10 19:21:28 -0500

    finished updating shuttle status

--- script.js
+++ script.js
@@ -19,12 +19,33 @@ window.addEventListener("load", function () {
 
       let faultyItems = document.getElementById("faultyItems");
       faultyItems.style.visibility = "visible";
+
       let pilotStatus = document.getElementById("pilotStatus");
       let copilotStatus = document.getElementById("copilotStatus");
       pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`
       copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch.`
 
-
+      let fuelStatus = document.getElementById("fuelStatus");
+      let launchBool = true;
+      if (fuelLevel.value < 10000) {
+         fuelStatus.innerHTML = "Fuel level too low for launch.";
+         launchBool = false;
+      }
+      
+      let cargoStatus = document.getElementById("cargoStatus");
+      if (cargoMass.value > 10000) {
+         cargoStatus.innerHTML = "Cargo level too high for launch."
+         launchBool = false;
+      }
+      
+      let launchStatus = document.getElementById("launchStatus");
+      if (!launchBool) {
+         launchStatus.innerHTML = "Shuttle not ready for launch"
+         launchStatus.style.color = "red";
+      } else {
+         launchStatus.innerHTML = "Shuttle is ready for launch"
+         launchStatus.style.color = "green";
+      }
    });
 
 });

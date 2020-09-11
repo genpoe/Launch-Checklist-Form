458807a0ab34c5cbe2a9bb13e7466ed480c37c41
Gil Marsh <marsh.skc@gmail.com>
2020-09-10 19:07:56 -0500

    submitting updates faultyItems names

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName");
+      let fuelLevel = document.querySelector("input[name=fuelLevel");
+      let cargoMass = document.querySelector("input[name=cargoMass");
+
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required.");
+         event.preventDefault();
+      } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+         alert("Please enter valid Fuel Level and Cargo Mass.");
+         event.preventDefault();
+      }
+
+      let faultyItems = document.getElementById("faultyItems");
+      faultyItems.style.visibility = "visible";
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`
+      copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch.`
+
+
+   });
+
+});
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
